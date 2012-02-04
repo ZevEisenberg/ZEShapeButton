e05d5bafae946f7d0fa5af6095abf7de9a33a870
Zev Eisenberg <igeek1@gmail.com>
2012-02-04 17:14:04 -0500

    adding main files

---
+++ ZEShapeButton.h
@@ -0,0 +1,12 @@
+//
+//  Created by Zev Eisenberg on 2/4/12.
+//  Copyright (c) 2012 Zev Eisenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZEShapeButton : UIButton
+
+@property (nonatomic, strong) UIBezierPath *shape;
+
+@end
---
+++ ZEShapeButton.m
@@ -0,0 +1,26 @@
+//
+//  Created by Zev Eisenberg on 2/4/12.
+//  Copyright (c) 2012 Zev Eisenberg. All rights reserved.
+//
+
+#import "ZEShapeButton.h"
+
+@implementation ZEShapeButton
+
+@synthesize shape;
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    if (!self.shape)
+    {
+        return [super hitTest:point withEvent:event];
+    }
+    UIView *hitView = [super hitTest:point withEvent:event];
+    if (hitView == self && [self.shape containsPoint:point])
+    {
+        return self;
+    }
+    return nil;
+}
+
+@end
